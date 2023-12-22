03634cef3f32ce48903e4c6f169a840dc099cc71
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-12-22 14:38:50 +0800

    Memory Mapping Changes

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -347,6 +347,6 @@ class DefaultConfig(n: Int = 1) extends Config(
   new WithNKBL3(4 * 1024, inclusive = false, banks = 4, ways = 8, core_num = n)
     ++ new WithNKBL2(256, inclusive = false, banks = 2, ways = 8, alwaysReleaseData = true)
     ++ new WithNKBL1D(64)
-    ++ new BaseConfig(n)
+    ++ new BaseConfig(n, true)
 )
 
